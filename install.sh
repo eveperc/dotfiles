2f88a106274b3e1f58758ea32ed1e5f48716704b
katsunori-ibusuki-yoriso <katsunori-ibusuki@yoriso.com>
2026-03-22 10:47:54 +0900

    feat: sketchybarの設定ファイルを追加

--- install.sh
+++ install.sh
@@ -78,6 +78,7 @@ setup_symlinks() {
   create_symlink "$DOTFILES_DIR/.config/nvim" "$HOME/.config/nvim"
   create_symlink "$DOTFILES_DIR/.config/fish" "$HOME/.config/fish"
   create_symlink "$DOTFILES_DIR/.config/wezterm" "$HOME/.config/wezterm"
+  create_symlink "$DOTFILES_DIR/.config/sketchybar" "$HOME/.config/sketchybar"
 
   # Linux固有
   if [[ "$OS" == "Linux" ]]; then
